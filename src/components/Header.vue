d322bd0ca91dca17b3cd30c0b8aa3fd97b24bea1
Gustavo Neitzke <gutoneitzke12345@gmail.com>
2022-11-19 10:29:13 -0300

    feat: add sass and new header to project

---
+++ src/components/Header.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+  <div class="content">
+    <div class="box-image"></div>
+    <h1>Olá! Meu nome é <strong>Gustavo Neitzke</strong></h1>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .content{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 2rem;
+    padding: 2rem;
+    .box-image{
+      height: 70px;
+      width: 70px;
+      background-color: $green;
+      border-radius: 50%;
+    }
+    h1{
+      color: $white;
+      margin: 0;
+      strong{
+        color: $green;
+      }
+    }
+  }
+</style>
