9e7f6e8ef4562973e8ad0c3a1253e0569908b25b
Gustavo Neitzke <gutoneitzke12345@gmail.com>
2022-11-23 22:46:00 -0300

    style: new adjusts to layout

--- src/App.vue
+++ src/App.vue
@@ -9,12 +9,27 @@ import SocialNetwork from './components/SocialNetwork.vue'
       <Header/>
     </header>
     <main>
-      <div class="content--left">
+      <div class="my-description">
         <img src="desenho.png" alt="">
-        <div class="left--informations">
+        <div class="informations">
           <h2>Gustavo Neitzke</h2>
-          <p>Sit sit deserunt pariatur dolor do deserunt cupidatat labore minim Lorem consequat sunt. Minim magna quis amet et ut magna ad pariatur do nostrud id. Mollit do ipsum laboris elit officia amet sunt.</p>
-          <p>Sit sit deserunt pariatur dolor do deserunt cupidatat labore minim Lorem consequat sunt. Minim magna quis amet et ut magna ad pariatur do nostrud id. Mollit do ipsum laboris elit officia amet sunt.</p>
+          <div class="informations--data">
+            <p>Sit sit deserunt pariatur dolor do deserunt cupidatat labore minim Lorem consequat sunt. Minim magna quis amet et ut magna ad pariatur do nostrud id. Mollit do ipsum laboris elit officia amet sunt.</p>
+            <p>Sit sit deserunt pariatur dolor do deserunt cupidatat labore minim Lorem consequat sunt. Minim magna quis amet et ut magna ad pariatur do nostrud id. Mollit do ipsum laboris elit officia amet sunt.</p>
+          </div>
+          <div class="informations--languages">
+            <span>HTML</span>
+            <span>CSS</span>
+            <span>JavaScript</span>
+            <span>Typescript</span>
+            <span>Vue.js</span>
+            <span>React.js</span>
+            <span>Node.js</span>
+            <span>PHP</span>
+            <span>Laravel</span>
+            <span>Mysql</span>
+            <span>Mariadb</span>
+          </div>
         </div>
       </div>
       <SocialNetwork />
@@ -30,14 +45,42 @@ import SocialNetwork from './components/SocialNetwork.vue'
       align-items: center;
       gap: 2rem;
       margin: 2rem;
-      min-height: 50vh;
-      .content--left{
+      .my-description{
         display: flex;
         align-items: center;
         gap: 1rem;
-        h2{
-          font-size: 3rem;
+        .informations{
+          display: flex;
+          flex-direction: column;
+          gap: 2rem;
+          h2{
+            font-size: 3rem;
+            margin: 0;
+          }
+          .informations--languages{
+            display: flex;
+            justify-content: flex-start;
+            flex-wrap: wrap;
+            gap: 1rem;
+            span{
+              background-color: $gray;
+              padding: .1rem 1rem;
+              font-size: .8rem;
+              border-radius: 1rem;
+              transition: .4s;
+              cursor: default;
+            }
+            span:hover{
+              background-color: $green;
+            }
+          }
         }
+      }
+    }
+    footer{
+      margin: 2rem;
+      h4{
+        font-weight: 100;
       }
     }
   }
