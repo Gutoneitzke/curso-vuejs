a71809f25a08668fbfd93e47ef89d335e703e1a4
Gustavo Neitzke <gutoneitzke12345@gmail.com>
2022-12-02 22:53:33 -0300

    feat: new animation to card and new carousel to my project

--- src/components/Repos.vue
+++ src/components/Repos.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import axios from 'axios'
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Slide, Navigation } from 'vue3-carousel'
 
 let repos = ref<Object>([])
 
@@ -16,39 +18,71 @@ const getReposFromGithub = () => {
     })
 }
 
+const transformCard = (data: any) => {
+  data.transform = !data.transform ? false : true
+  data.transform = !data.transform
+}
+
 getReposFromGithub()
 
 </script>
 
 <template>
   <div v-if="repos" class="repos">
-    <div v-for="(repo,i) in repos" :key="i" class="repos--repo">
-      <p v-text="repo.name"></p>
-    </div>
+    <carousel :items-to-show="5">
+      <slide v-for="(repo,i) in repos" :key="i" :class="['repos--repo', !repo.transform ? '' : 'rotate']" @mouseout="!repo.transform ? '' : transformCard(repo)" @click="transformCard(repo)">
+        <div v-if="!repo.transform">
+          <p v-text="repo.name"></p>
+        </div>
+        <div v-else>
+          <p v-text="repo.stargazers_count"></p>
+        </div>
+      </slide>
+
+      <template #addons>
+        <navigation />
+      </template>
+    </carousel>
   </div>
   <p v-else>Nenhum registro</p>
 </template>
 
-<style lang="scss" scoped>
+<style lang="scss">
   .repos{
-    display: flex;
-    align-items: center;
-    gap: 1rem;
-    .repos--repo{
-      min-width: 140px;
-      min-height: 140px;
-      max-width: 140px;
-      max-height: 140px;
-      background: linear-gradient(to bottom, $green, $dark2);
-      border-radius: 1rem;
-      padding: 1rem;
-      box-sizing: border-box;
-      display: flex;
-      justify-content: center;
-      align-items: flex-end;
-      p{
-        text-align: center;
+    .carousel{
+      .carousel__viewport{
+        .carousel__track{
+          display: flex;
+          align-items: center;
+          gap: 1rem !important;
+          .repos--repo{
+            min-width: 140px;
+            min-height: 140px;
+            max-width: 140px;
+            max-height: 140px;
+            background: linear-gradient(to bottom, $green, $dark2);
+            border-radius: 1rem;
+            padding: 1rem;
+            box-sizing: border-box;
+            display: flex;
+            justify-content: center;
+            align-items: flex-end;
+            p{
+              text-align: center;
+              text-transform: capitalize;
+            }
+          }
+          .rotate{
+            animation: rotatecard;
+            animation-duration: 1s;
+            background: linear-gradient(to bottom, $dark, $dark2);
+          }
+        }
       }
     }
+  }
+  @keyframes rotatecard {
+    from {transform: rotateY(0deg);}
+    to {transform: rotateY(360deg);}
   }
 </style>
